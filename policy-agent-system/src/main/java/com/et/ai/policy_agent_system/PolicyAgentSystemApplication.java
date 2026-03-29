749ad2ee83f6abcd37800a74f3ca594a5526b54f
sudhirskp <gnitwallah@gmail.com>
2026-03-29 10:35:04 +0530

    Set HTTPS protocols to TLSv1.2 for enhanced security

--- policy-agent-system/src/main/java/com/et/ai/policy_agent_system/PolicyAgentSystemApplication.java
+++ policy-agent-system/src/main/java/com/et/ai/policy_agent_system/PolicyAgentSystemApplication.java
@@ -1,5 +1,6 @@
 package com.et.ai.policy_agent_system;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PolicyAgentSystemApplication {
 
 	public static void main(String[] args) {
+        System.setProperty("https.protocols", "TLSv1.2");
 		SpringApplication.run(PolicyAgentSystemApplication.class, args);
 	}
 
